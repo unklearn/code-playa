737901f0b9dfd7a79c5229e6ec9841f5ac221159
Tharun Mathew Paul <tmpaul06@gmail.com>
2020-01-13 17:05:09 +0530

    Add about section

--- src/App.js
+++ src/App.js
@@ -14,6 +14,16 @@ function App() {
         Codeplaya
       </header>
       <Sidebar>
+          <h4>About</h4>
+          <p>
+            Codeplaya is a code playback player inspired by the likes of <a href='https://asciinema.org/' target='_blank'>asciinema.</a>
+          </p>
+          <p>
+            The project source is located <a href='https://github.com/unklearn/code-playa' target='_blank'>here</a>
+          </p>
+          <p>
+            &copy; TMP
+          </p>
       </Sidebar>
       <main>
         <CodeMirrorPlayer editing/>
